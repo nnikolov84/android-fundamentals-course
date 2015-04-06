5fe6d76c4b605553296b1a396392a7b5a6ea838c
Nikola Nikolov <nikolameister@gmail.com>
2015-04-06 09:30:27 +0300

    test4

--- app/src/main/java/org/hackafe/sunshine/DBAdapter.java
+++ app/src/main/java/org/hackafe/sunshine/DBAdapter.java
@@ -25,12 +25,6 @@ public class DBAdapter {
     // DB Fields
     public static final String KEY_ROWID = "_id";
     public static final int COL_ROWID = 0;
-    String dt;
-    String dt3;
-
-
-
-
     /*
      * CHANGE 1:
      */
